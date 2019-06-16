2fe1ba8505740a2612314333423e1275166aba6b
franpog859 <franciszek.pog@gmail.com>
2019-06-16 15:00:38 +0200

    Fix bug with the elevation

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ import nvector as nv
 from tqdm import tqdm
 
 
+def main():
+    input_file = 'srtm_40_02.tif'
+    step = 100 # TODO: Add counting step function to be relative to the image size
+
+    print("Reading lat long elevation data from the {} file...".format(input_file))
+    lat_long_elev_data = get_geocoords_from_input(input_file, step)
+
+    print("Converting lat long elevation data to xyz vectors...")
+    vectors = geocoords_to_vectors(lat_long_elev_data)
+
+    print("Marking tops of the world...")
+    tops_of_the_world = get_tops_of_the_world(vectors)
+
+    print("Saving output file...")
+    save_file(input_file, tops_of_the_world, step)
+
+    print("Tops of the world saved successfully!")
+
+
 def get_geocoords_from_input(input_file, step):
     with rasterio.open(input_file) as src:
         plot.show(src)
@@ -15,8 +34,8 @@ def get_geocoords_from_input(input_file, step):
         for y in range(0, src.height, step): 
             # Range is limited due to long execution time. for x in range(src.width):
             for x in range(0, src.width, step): 
-                longitude, latitude = src.xy(x,y)
-                elevation = elevation_values[x,y]
+                longitude, latitude = src.xy(y,x)
+                elevation = elevation_values[y,x]
                 elevation = elevation if elevation > -100 else 0 # Cleaning sea data
                 lat_long_elev_data.append((
                     x, y, latitude, longitude, elevation
@@ -28,8 +47,9 @@ def geocoords_to_vectors(lat_long_elev_data):
     wgs84 = nv.FrameE(name='WGS84')
     vectors = []
     for width, height, latitude, longitude, elevation in lat_long_elev_data:
+        depth = elevation * (-1)
         pointB = wgs84.GeoPoint(
-            latitude=latitude, longitude=longitude, z=elevation, degrees=True
+            latitude=latitude, longitude=longitude, z=depth, degrees=True
         )
         p_EB_E = pointB.to_ecef_vector()
         x, y, z = p_EB_E.pvector.ravel()[0], p_EB_E.pvector.ravel()[1], p_EB_E.pvector.ravel()[2]
@@ -99,7 +119,7 @@ def save_file(input_file, tops_of_the_world, step):
             if y+i < height:
                 for j in range(step):
                     if x+j < width:
-                        data[0][x+j][y+i] = 32000
+                            data[0][y+i][x+j] = 32000
 
     with rasterio.open('output.tif', 'w', **profile) as dst:
         dst.write(data[0].astype(rasterio.int16), 1) 
@@ -112,25 +132,6 @@ def save_file(input_file, tops_of_the_world, step):
     plot.show(data)
 
 
-def main():
-    input_file = 'srtm_40_02.tif'
-    step = 100 # TODO: Add counting step function to be relative to the image size
-
-    print("Reading lat long elevation data from the {} file...".format(input_file))
-    lat_long_elev_data = get_geocoords_from_input(input_file, step)
-
-    print("Converting lat long elevation data to xyz vectors...")
-    vectors = geocoords_to_vectors(lat_long_elev_data)
-
-    print("Marking tops of the world...")
-    tops_of_the_world = get_tops_of_the_world(vectors)
-
-    print("Saving output file...")
-    save_file(input_file, tops_of_the_world, step)
-
-    print("Tops of the world saved successfully!")
-    
-   
 if __name__ == "__main__":
     main()
         
