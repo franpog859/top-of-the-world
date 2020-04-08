5196c9bbf2606b4e7a4cb268896829de675caeee
franpog859 <franciszek.pog@gmail.com>
2020-04-08 13:29:15 +0200

    Refactor code even more

--- print_globe.py
+++ print_globe.py
@@ -2,15 +2,13 @@ from __future__ import annotations
 import matplotlib.pyplot as plt
 import numpy as np
 import rasterio
-import rasterio.plot as plot
 import nvector as nv
-from mpl_toolkits import mplot3d
 from tqdm import tqdm
 
 
 def main():
-    input_file = 'data/srtm_40_02.tif' # TODO: Do not hardcode it. Use a script parameter
-    step = 180 # TODO: Add counting step function to be relative to the image size
+    input_file = 'data/srtm_40_02.tif'  # TODO: Do not hardcode it. Use a script parameter
+    step = 180  # TODO: Add counting step function to be relative to the image size
 
     print("Reading lat long elevation data from the {} file...".format(input_file))
     latlongelev_list = get_latlongelev_list_from_tif_image(input_file, step)
@@ -28,6 +26,7 @@ def main():
 
     # print("Tops of the world saved successfully!")
 
+
 def plotGlobe(xyz_list: list(XYZ)):
     x = []
     y = []
@@ -43,16 +42,18 @@ def plotGlobe(xyz_list: list(XYZ)):
 
 
 class LatLongElev:
-    def __init__(self, latitude: float, longitude: float, elevation: float=0):
+    def __init__(self, latitude: float, longitude: float, elevation: float=0.0):
         self.latitude = latitude
         self.longitude = longitude
         self.elevation = elevation
+
 
 class XYZ:
     def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
+
     def project_onto_line(self, _a: XYZ, _b: XYZ) -> XYZ:
         # See https://gamedev.stackexchange.com/questions/72528/how-can-i-project-a-3d-point-onto-a-3d-line
         p = self.to_np_array()
@@ -60,8 +61,9 @@ class XYZ:
         b = _b.to_np_array()
         ap = p-a
         ab = b-a
-        result = a + np.dot(ap,ab)/np.dot(ab,ab) * ab
+        result = a + np.dot(ap, ab)/np.dot(ab, ab) * ab
         return XYZ(result[0], result[1], result[2])
+
     def to_np_array(self) -> np.array:
         return np.array([self.x, self.y, self.z])
 
@@ -71,10 +73,10 @@ def get_latlongelev_list_from_tif_image(image_file: str, step: int=1) -> list(La
     latlongelev_list = []
     with rasterio.open(image_file) as image:
         elevation_values = image.read(1)
-        for y in range(0, image.height, step): 
-            for x in range(0, image.width, step): 
-                longitude, latitude = image.xy(y,x)
-                elevation = elevation_values[y,x]
+        for y in range(0, image.height, step):
+            for x in range(0, image.width, step):
+                longitude, latitude = image.xy(y, x)
+                elevation = elevation_values[y, x]
                 # Clean under the sea elevations
                 elevation = elevation if elevation > DEEPEST_DEPRESSION_ON_EARTH else DEEPEST_DEPRESSION_ON_EARTH
                 latlongelev_list.append(LatLongElev(latitude, longitude, elevation))
@@ -94,12 +96,11 @@ def latlongelev_list_to_xyz_list(latlongelev_list: list(LatLongElev)) -> list(XY
     return xyz_list
 
 
-CENTER_OF_THE_EARTH = XYZ(0, 0, 0)
 def filter_only_tops(xyz_list: list(XYZ)) -> list(XYZ):
     progress_bar = tqdm(total=len(xyz_list))
     top_list = []
     for xyz in xyz_list:
-        top = get_top_for_direction(CENTER_OF_THE_EARTH, xyz, xyz_list)
+        top = _get_top_for_direction(xyz, xyz_list)
         # Store only unique tops
         if top not in top_list: 
             top_list.append(top)
@@ -108,17 +109,19 @@ def filter_only_tops(xyz_list: list(XYZ)) -> list(XYZ):
     return top_list
 
 
-def get_top_for_direction(center: XYZ, direction: XYZ, xyz_list: list(XYZ)) -> XYZ: 
+CENTER_OF_THE_EARTH = XYZ(0, 0, 0)
+def _get_top_for_direction(direction: XYZ, xyz_list: list(XYZ)) -> XYZ:
     xyz_projection_list = []
     for xyz in xyz_list:
-        xyz_projection = xyz.project_onto_line(center, direction)
-        if are_on_the_same_side_relative_to_center(xyz_projection, direction):
+        xyz_projection = xyz.project_onto_line(CENTER_OF_THE_EARTH, direction)
+        if _are_on_the_same_side_relative_to_center(xyz_projection, direction):
             xyz_projection_list.append(xyz_projection)
 
-    top = max(xyz_projection_list, key=distance)
+    top = max(xyz_projection_list, key=_distance)
     return top
 
-def are_on_the_same_side_relative_to_center(a: XYZ, b: XYZ) -> bool:
+
+def _are_on_the_same_side_relative_to_center(a: XYZ, b: XYZ) -> bool:
     signs = np.sign([a.x, b.x, a.y, b.y, a.z, b.z])
     for axis in range(3):
         if signs[0+axis] == signs[1+axis]:
@@ -126,31 +129,10 @@ def are_on_the_same_side_relative_to_center(a: XYZ, b: XYZ) -> bool:
     return False
 
 
-def distance(xyz: XYZ) -> float:
+def _distance(xyz: XYZ) -> float:
     # The square root function is monotonic, so it can be discarded
     return xyz.x**2 + xyz.y**2 + xyz.z**2
 
 
-def save_file(input_file, tops_of_the_world, step):
-    with rasterio.open(input_file) as src:
-        profile = src.profile
-        data = src.read()
-        width = src.width
-        height = src.height
-
-    for x, y in tops_of_the_world:
-        for i in range(step):
-            if y+i < height:
-                for j in range(step):
-                    if x+j < width:
-                            data[0][y+i][x+j] = 32000
-
-    with rasterio.open('output.tif', 'w', **profile) as dst:
-        dst.write(data[0].astype(rasterio.int16), 1) 
-
-    plot.show(data)
-
-
 if __name__ == "__main__":
     main()
-        
