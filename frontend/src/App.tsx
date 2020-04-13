df6589f1a9cebb20a39523e3a35dbee217e163ac
franpog859 <franciszek.pog@gmail.com>
2020-04-13 18:27:23 +0200

    Add buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
-  const buttonClick = () => {
+  const openMap = () => {
     window.open("https://www.google.be/maps/dir/51.3867197,28.722/53.296,29.1225/data=!3m1!4b1!4m2!4m1!3e2");
+  }
+
+  const openAbout = () => {
+    window.open("https://github.com/franpog859/top-of-the-world/blob/Add-frontend-boilerplate/README.md");
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={buttonClick}>Take me to the top!</button>
+        <Button variant="success" onClick={openMap} size="lg">Take me to the top!</Button><br></br>
+        <Button variant="outline-secondary" onClick={openAbout} size="lg">Wait, what?</Button>
       </header>
     </div>
   );
