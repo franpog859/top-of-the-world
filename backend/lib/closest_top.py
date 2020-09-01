c2dfd0cc38bd6fd8a24935a48821af31b461cf61
franpog859 <franciszek.pog@gmail.com>
2020-09-01 13:59:38 +0200

    Clean up the code and test the functionality

--- backend/lib/closest_top.py
+++ backend/lib/closest_top.py
@@ -17,17 +17,18 @@ def find_closest_top(latitude: float, longitude: float, collection: pymongo.coll
 
 def convert_latlong_to_xyz(latitude: float, longitude: float) -> np.ndarray:
     # See https://github.com/pbrod/nvector#example-4-geodetic-latitude-to-ecef-vector
-    wgs84 = nv.FrameE(name='WGS84')
-    pointB = wgs84.GeoPoint(latitude=latitude, longitude=longitude, z=0.0, degrees=True)
-    xyz = pointB.to_ecef_vector().pvector.ravel()
+    xyz = nv.FrameE(name='WGS84') \
+            .GeoPoint(latitude=latitude, longitude=longitude, z=0.0, degrees=True) \
+            .to_ecef_vector() \
+            .pvector.ravel()
     return xyz
 
 
 # TODO:
 def calculate_indexes_for_xyz(xyz: np.ndarray, chunk_size: int) -> List[np.ndarray]:
-    x = xyz[0] - xyz[0] % chunk_size
-    y = xyz[1] - xyz[1] % chunk_size
-    z = xyz[2] - xyz[2] % chunk_size
+    x = xyz[0] - xyz[0] % np.int(chunk_size)
+    y = xyz[1] - xyz[1] % np.int(chunk_size)
+    z = xyz[2] - xyz[2] % np.int(chunk_size)
     return [np.array([x, y, z])]
 
 
@@ -38,14 +39,14 @@ def get_tops_for_index(xyz: np.ndarray, collection: pymongo.collection.Collectio
 
 
 def calculate_closest_top(xyz: np.ndarray, tops: List[np.ndarray]) -> np.ndarray:
-    # The square root function is monotonic, so it can be discarded
-    closest_top = min(tops, key=lambda item: ((xyz[0]-item[0])**2 + (xyz[1]-item[1])**2 + (xyz[2]-item[2])**2))
+    closest_top = min(tops, key=lambda item: np.linalg.norm(xyz - item))
     return closest_top
 
 
 def convert_xyz_to_latlong(xyz: np.ndarray) -> Tuple[float, float]:
     # See https://github.com/pbrod/nvector#example-3-ecef-vector-to-geodetic-latitude
-    wgs84 = nv.FrameE(name='WGS84')
     position_B = 6371e3 * np.vstack((xyz[0], xyz[1], xyz[2]))
-    latitude, longitude, _ = wgs84.ECEFvector(position_B).to_geo_point().latlon_deg
-    return latitude, longitude
+    latitude, longitude, _ = nv.FrameE(name='WGS84') \
+                               .ECEFvector(position_B) \
+                               .to_geo_point().latlon_deg
+    return float(latitude), float(longitude)
