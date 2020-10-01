dd5a57453b82670299de3ec95b1973a7097d44a7
franpog859 <franciszek.pog@gmail.com>
2020-10-01 10:50:17 +0200

    Make the code pretty

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,102 +1,120 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import Lead from './Lead/Lead'
-import Buttons from './Buttons/Buttons'
-import Footer from './Footer/Footer'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Lead from "./Lead/Lead";
+import Buttons from "./Buttons/Buttons";
+import Footer from "./Footer/Footer";
 
 interface simplePosition {
-  latitude: number
-  longitude: number
+  latitude: number;
+  longitude: number;
 }
 
 function App() {
-  const [currentPosition, setCurrentPosition] = useState<Position | undefined>(undefined)
-  const [topPosition, setTopPosition] = useState<simplePosition | undefined>(undefined)
-  const [isMapButtonClicked, setIsMapButtonClicked] = useState<boolean>(false)
-  const [didFetchingFail, setDidFetchingFail] = useState<boolean>(false)
-  const backendURL = process.env.REACT_APP_BACKEND_URL!
-  const authToken = process.env.REACT_APP_AUTH_TOKEN!
+  const [currentPosition, setCurrentPosition] = useState<Position | undefined>(
+    undefined
+  );
+  const [topPosition, setTopPosition] = useState<simplePosition | undefined>(
+    undefined
+  );
+  const [isMapButtonClicked, setIsMapButtonClicked] = useState<boolean>(false);
+  const [didFetchingFail, setDidFetchingFail] = useState<boolean>(false);
+  const backendURL = process.env.REACT_APP_BACKEND_URL!;
+  const authToken = process.env.REACT_APP_AUTH_TOKEN!;
 
   const setAsyncPosition = async () => {
     navigator.geolocation.getCurrentPosition(
       position => setCurrentPosition(position),
-      error => console.error('Failed to get current position: ', error)
-    )
-  }
-  const buildHealthzBackendPath = () => `${backendURL}/healthz`
+      error => console.error("Failed to get current position: ", error)
+    );
+  };
+  const buildHealthzBackendPath = () => `${backendURL}/healthz`;
   const warmUpBackend = async () => {
-    console.log('Warming the backend up...')
-    fetch(buildHealthzBackendPath(), {headers: {'token': authToken}})
+    console.log("Warming the backend up...");
+    fetch(buildHealthzBackendPath(), { headers: { token: authToken } })
       .then(response => response.json())
       .then(response => console.debug(response))
-      .catch(error => console.error('Failed to warm up the backend: ', error))
-  }
+      .catch(error => console.error("Failed to warm up the backend: ", error));
+  };
   useEffect(() => {
-    setAsyncPosition()
-    warmUpBackend()
-  }, [])
+    setAsyncPosition();
+    warmUpBackend();
+  }, []);
 
   const buildClosestTopBackendPath = (position: Position) => {
-    return `${backendURL}/closestTop/${position.coords.latitude}/${position.coords.longitude}`
-  }
+    return `${backendURL}/closestTop/${position.coords.latitude}/${position.coords.longitude}`;
+  };
   const fetchClosestTopPosition = async () => {
-    console.log('Fetching closest top position...')
-    fetch(buildClosestTopBackendPath(currentPosition!), {headers: {'token': authToken}})
+    console.log("Fetching closest top position...");
+    fetch(buildClosestTopBackendPath(currentPosition!), {
+      headers: { token: authToken }
+    })
       .then(response => response.json())
-      .then(response => {console.debug(response); return response})
-      .then(response => setTopPosition({
-        latitude: response.latitude,
-        longitude: response.longitude
-      }))
-      .catch(error => {
-        console.error('Failed to get closest top: ', error)
-        setDidFetchingFail(true)
-        setIsMapButtonClicked(false)
+      .then(response => {
+        console.debug(response);
+        return response;
       })
-  }
+      .then(response =>
+        setTopPosition({
+          latitude: response.latitude,
+          longitude: response.longitude
+        })
+      )
+      .catch(error => {
+        console.error("Failed to get closest top: ", error);
+        setDidFetchingFail(true);
+        setIsMapButtonClicked(false);
+      });
+  };
   useEffect(() => {
-    if (currentPosition) fetchClosestTopPosition()
-  }, [currentPosition])
+    if (currentPosition) fetchClosestTopPosition();
+  }, [currentPosition]);
 
   const openMap = () => {
-    const currentLatitude = currentPosition!.coords.latitude.toString()
-    const currentLongitude = currentPosition!.coords.longitude.toString()
-    const topLatitude = topPosition!.latitude.toString()
-    const topLongitude = topPosition!.longitude.toString()
-    window.open(`https://www.google.be/maps/dir/${currentLatitude},${currentLongitude}`+
-      `/${topLatitude},${topLongitude}/data=!3m1!4b1!4m2!4m1!3e2`)
-  }
+    const currentLatitude = currentPosition!.coords.latitude.toString();
+    const currentLongitude = currentPosition!.coords.longitude.toString();
+    const topLatitude = topPosition!.latitude.toString();
+    const topLongitude = topPosition!.longitude.toString();
+    window.open(
+      `https://www.google.be/maps/dir/${currentLatitude},${currentLongitude}` +
+        `/${topLatitude},${topLongitude}/data=!3m1!4b1!4m2!4m1!3e2`
+    );
+  };
   useEffect(() => {
-    if (currentPosition !== undefined &&
-        topPosition !== undefined &&
-        isMapButtonClicked === true) {
-
-          setIsMapButtonClicked(false)
-          openMap()
-        }
-  }, [currentPosition, topPosition, isMapButtonClicked])
+    if (
+      currentPosition !== undefined &&
+      topPosition !== undefined &&
+      isMapButtonClicked === true
+    ) {
+      setIsMapButtonClicked(false);
+      openMap();
+    }
+  }, [currentPosition, topPosition, isMapButtonClicked]);
 
   const clickMapButton = () => {
-    setIsMapButtonClicked(true)
-  }
+    setIsMapButtonClicked(true);
+  };
   const openAbout = () => {
-    window.open('https://github.com/franpog859/top-of-the-world/blob/master/README.md')
-  }
+    window.open(
+      "https://github.com/franpog859/top-of-the-world/blob/master/README.md"
+    );
+  };
 
   return (
-    <div className='App'>
-      <header className='App-header App-animation'>
-        <Lead 
+    <div className="App">
+      <header className="App-header App-animation">
+        <Lead
           didFetchingFail={didFetchingFail}
-          isLocationAvailable={currentPosition !== undefined}/>
+          isLocationAvailable={currentPosition !== undefined}
+        />
         <Buttons
           clickMapButton={clickMapButton}
           clickAboutButton={openAbout}
-          isMapButtonClicked={isMapButtonClicked}/>
+          isMapButtonClicked={isMapButtonClicked}
+        />
         <Footer />
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Lead/Lead.tsx
+++ frontend/src/Lead/Lead.tsx
@@ -1,44 +1,59 @@
-import React from 'react'
-import './Lead.css'
+import React from "react";
+import "./Lead.css";
 import shortid from "shortid";
-import { Icon } from '@iconify/react';
-import bxsLockAlt from '@iconify/icons-bx/bxs-lock-alt';
-import emojiSad from '@iconify/icons-heroicons-solid/emoji-sad';
+import { Icon } from "@iconify/react";
+import bxsLockAlt from "@iconify/icons-bx/bxs-lock-alt";
+import emojiSad from "@iconify/icons-heroicons-solid/emoji-sad";
 
 interface LeadProps {
-  didFetchingFail: boolean
-  isLocationAvailable: boolean
+  didFetchingFail: boolean;
+  isLocationAvailable: boolean;
 }
 
-function Lead({didFetchingFail, isLocationAvailable}:LeadProps) {
+function Lead({ didFetchingFail, isLocationAvailable }: LeadProps) {
+  const textNormal = (
+    <>
+      <p>
+        Want to go to THE TOP OF THE WORLD but you're too lazy to even think
+        about the Himalayas?
+      </p>
+      <p>Well, I've got something for you!</p>
+    </>
+  );
 
-  const textNormal = <>
-    <p>Want to go to THE TOP OF THE WORLD but you're too lazy to even think about the Himalayas?</p>
-    <p>Well, I've got something for you!</p>
-  </>
+  const textNoLocation = (
+    <>
+      <p>Allow the location!</p>
+      <p>
+        Click on the <Icon icon={bxsLockAlt} className="Lead-icon" /> near the
+        website address!
+      </p>
+    </>
+  );
 
-  const textNoLocation = <>
-    <p>Allow the location!</p>
-    <p>Click on the <Icon icon={bxsLockAlt} className='Lead-icon' /> near the website address!</p>
-  </>
-
-  const textFailedToFetch = <>
-    <p>Something went wrong <Icon icon={emojiSad} className='Lead-icon' /></p>
-    <p>Try to refresh the app!</p>
-  </>
+  const textFailedToFetch = (
+    <>
+      <p>
+        Something went wrong <Icon icon={emojiSad} className="Lead-icon" />
+      </p>
+      <p>Try to refresh the app!</p>
+    </>
+  );
 
   // See https://stackoverflow.com/questions/57615274/my-animation-is-not-working-when-re-rending-my-react-component
-  const getRandomKey = () => shortid.generate()
+  const getRandomKey = () => shortid.generate();
 
   return (
-    <div className='Lead' data-testid='Lead'>
-      <div 
-        className='Lead-div Lead-animation'
-        key={getRandomKey()}>
-          {!isLocationAvailable ? textNoLocation : (!didFetchingFail ? textNormal : textFailedToFetch)}
+    <div className="Lead" data-testid="Lead">
+      <div className="Lead-div Lead-animation" key={getRandomKey()}>
+        {!isLocationAvailable
+          ? textNoLocation
+          : !didFetchingFail
+          ? textNormal
+          : textFailedToFetch}
       </div>
     </div>
-  )
+  );
 }
 
-export default Lead
+export default Lead;
